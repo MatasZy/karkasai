b7ac39ae3b4c8fbaebb8ef4296f4e766d14d6c7d
mataszy <matas.zymantas@gmail.com>
2023-05-28 22:25:13 +0300

    penktas commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+
+                <div class="text-center">
+                    <h1 class="text-4xl font-bold">Welcome to my project!</h1>
 
                 </div>
             </div>
